54821c892e89fe9147aae43625217195c0885003
Lexuz <90095821+Lexuz17@users.noreply.github.com>
2023-07-31 22:56:58 +0700

    UI Home page 100% and UI User page 20%
    Belum terlalu responsif

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -3,6 +3,8 @@
     import { Head, Link, router } from '@inertiajs/vue3';
     import MainLayout from '@/Layouts/MainLayout.vue';
 
+    import LikesSection from '@/Components/LikesSection.vue'
+
     import 'vue3-carousel/dist/carousel.css'
     import { Carousel, Slide, Navigation } from 'vue3-carousel';
 
@@ -39,16 +41,66 @@
                 snapAlign="start"
             >
 
-                <Slide v-for="slide in 10" :key="slide" class="h-40">
+                <Slide v-for="slide in 10" :key="slide">
                     <Link href="/" class="relative mx-auto text-center mt-4 px-2 cursor-pointer">
                         <div class="absolute z-[-1] -top-[5px] left-[4px] rounded-full rotate-45 w-[64px] h-[64px] contrast-[1.3] bg-gradient-to-t from-yellow-300 to-purple-500 via-red-500" >
-
+                            <div class="rounded-full ml-[3px] mt-[3px] w-[58px] h-[58px] bg-white"/>
                         </div>
+                        <img class="object-cover rounded-full w-[56px] h-[56px] -mt-[1px]" src="https://images.squarespace-cdn.com/content/v1/582c3c949f745686020f3de9/1481706585962-ULV3HESIDCP5LXC2TDHI/title2.jpg?format=2500w" alt="KiminoNawa">
+                        <div class="text-xs mt-2 w-[60px] truncate overflow-hidden">NAME HERE</div>
                     </Link>
                 </Slide>
 
+                <template #addons>
+                    <Navigation />
+                </template>
             </Carousel>
+
+            <div id="Posts" class="px-4 max-w-[600px] mx-auto mt-10">
+                <div class="flex items-center justify-between py-2">
+                    <div class="flex items-center">
+                        <link href="/" class="flex items-center">
+                        <img src="https://theoddapple.com/wp-content/uploads/2020/11/i-want-to-eat-your-pancreas-2018-japanese-anime.jpg" alt="" class="object-cover rounded-full w-[38px] h-[38px]">
+                        <div class="ml-4 font-extrabold text-[15px]">NAME HERE</div>
+
+                        <div class="flex items-center text-[15px] text-gray-500">
+                            <span class="-mt-5 ml-2 mr-[5px] text-[35px]">.</span>
+                            <div>DATE HERE</div>
+                        </div>
+                    </div>
+
+                    <DotsHorizontal class="cursor-pointer" :size="27" />
+                </div>
+
+                <div class="bg-black rounded-lg w-full min-h-[400px] flex items-center">
+                    <img class="mx-auto w-full" src="https://www.arealme.com/cover-images/which-your-lie-in-april-character-are-you.png" alt="you lie in april">
+                </div>
+
+                <LikesSection/>
+
+                <div class="text-black font-extrabold py-1">3 likes</div>
+                <div>
+                    <span class="text-black font-extrabold">NAME HERE</span>
+                    this is some text here
+                </div>
+                <button class="text-gray-500 font-extrabold py-1">
+                    View all 4 comments
+                </button>
+            </div>
+
+            <div class="pb-20"></div>
         </div>
     </MainLayout>
-
 </template>
+
+<style>
+    .carousel__prev,
+    .carousel__next,
+    .carousel__prev:hover,
+    .carousel__next:hover{
+        color: rgb(49,49,49);
+        background-color: rgb(255, 255, 255);
+        border-radius: 100%;
+        margin: 0 20px;
+    }
+</style>
